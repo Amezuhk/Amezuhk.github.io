7ff27663cb9d2d02aa9b0a83ac042f687f2288ed
Khuzaima Mushtaq <33531300+Amezuhk@users.noreply.github.com>
2019-03-11 17:14:36 -0700

    Start screen

--- src/MyGame/MyGame.js
+++ src/MyGame/MyGame.js
@@ -18,14 +18,8 @@ function MyGame() {
     this.kCue = "assets/sounds/BlueLevel_cue.wav";
     // The camera to view the scene
     this.mCamera = null;
-    this.mShapes = new GameObjectSet();
-    this.mPlayer = this.mShapes.size();
     this.Button1 = null;
-    this.Button2 = null;
     this.mMsg = null;
-    this.mMouse = null;
-//    this.mSmoke = null;
-    this.ghost = null;
 }
 gEngine.Core.inheritPrototype(MyGame, Scene);
 
@@ -36,9 +30,8 @@ MyGame.prototype.loadScene = function () {
 
 MyGame.prototype.unloadScene = function () {
     gEngine.Textures.unloadTexture(this.kUIButton);
-//    var nextLevel = new Level(this.kLevelOneSceneFile, this.kLevelOneBgClip,
-//    this.kCue);
-    var nextLevel = new GameOver();
+    var nextLevel = new Level(this.kLevelOneSceneFile, this.kLevelOneBgClip,
+    this.kCue);
     gEngine.Core.startScene(nextLevel);
 };
 
@@ -53,27 +46,8 @@ MyGame.prototype.initialize = function () {
             // sets the background to gray
     gEngine.DefaultResources.setGlobalAmbientIntensity(3);
 
-    var g = new Player();
-    this.mShapes.addToSet(g);
-    
-    var l = new Renderable();
-    l.setColor([0,.5,.5,1]);
-    var xf2 = l.getXform();
-    xf2.setSize(10,2.5);
-    xf2.setPosition(50,20);
-    var g2 = new GameObject(l);
-    var r2 = new RigidRectangle(xf2,10,2.5);
-    r2.setMass(0);
-    g2.setRigidBody(r2);
-    g2.toggleDrawRigidShape();
-    this.mShapes.addToSet(g2);
     this.mMsg = new UIText("Brix",[500,650],20,1,0,[0,0,0,1]);
     this.mButton1 = new UIButton(this.kUIButton,this.STUFF1,this,[500,250],[300,100],"Start",8,[1,1,1,1],[0,0,0,1]);
-//    this.mSmoke = new Smoke(30,9,3,-30,3,0,5,0,20,0,1,3);
-    this.ghost = new Ghost();
-    this.mMouse = new MousePlatforms(this.mShapes,this.mCamera);
-    var pick = new PlatformPickup(70,30);
-    this.mMouse.addPickup(pick);
 };
 
 // This is the draw function, make sure to setup proper drawing environment, and more
@@ -83,35 +57,12 @@ MyGame.prototype.draw = function () {
     gEngine.Core.clearCanvas([0.9, 0.9, 0.9, 1.0]); // clear to light gray
     
     this.mCamera.setupViewProjection();
-    this.mShapes.draw(this.mCamera);
-    //this.mButton1.draw(this.mCamera);
-    //this.mMsg.draw(this.mCamera);
-    this.ghost.draw(this.mCamera);
-    this.mMouse.draw();
+    this.mButton1.draw(this.mCamera);
+    this.mMsg.draw(this.mCamera);
 };
 
 MyGame.prototype.update = function () {
-    this.mShapes.update();    
-    var pl = this.mShapes.getObjectAt(0);
-    //pl.checkFall();
-    var o = this.mShapes.getObjectAt(1);
-    var col = new CollisionInfo();
-    var ot = o.getRigidBody();
-    var status = pl.getRigidBody().collisionTest(ot,col);
-    if(status){
-        pl.setFall(false);
-    }
     this.mButton1.update();
-    gEngine.Physics.processCollision(this.mShapes,[]);
-    this.ghost.rotateObjPointTo(pl.getXform().getPosition(),1);
-    this.ghost.update();
-    var sp = [];
-    var change = pl.isAlive(sp,0,0,100,this.ghost);
-    change = pl.shakeOver();
-    if(change){
-        gEngine.GameLoop.stop();
-    }
-    this.mMouse.update(pl);
 };
 
 MyGame.prototype.STUFF1 = function(){
